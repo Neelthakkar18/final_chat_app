eb2b37aa43d0d067542f5c65e1888160d11a42fa
Neelthakkar18 <tneel9414@gmail.com>
2026-04-18 22:50:55 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta
-import json
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,13 +8,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    full_name = db.Column(db.String(100), default='')
-    bio = db.Column(db.String(200), default='Available')
-    profile_pic = db.Column(db.String(10), default='🎤')
-    phone = db.Column(db.String(20), default='')
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     is_online = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Message(db.Model):
     __tablename__ = 'message'
@@ -26,13 +20,3 @@ class Message(db.Model):
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     read = db.Column(db.Boolean, default=False)
     delivered = db.Column(db.Boolean, default=False)
-
-class Status(db.Model):
-    __tablename__ = 'status'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    content_type = db.Column(db.String(20), default='text')
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    expires_at = db.Column(db.DateTime, default=lambda: datetime.utcnow() + timedelta(hours=24))
-    views = db.Column(db.Text, default='[]')
